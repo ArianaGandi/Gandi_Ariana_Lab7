704c47692154db605f1abf38dfd32844f1684150
Ariana Gandi <ary_gandi@yahoo.com>
2022-11-23 13:40:10 +0200

    lab7

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Gandi_Ariana_Lab7;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
