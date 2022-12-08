0456261cc41031d46f46ac689301c6e67c1ab359
Ariana Gandi <ary_gandi@yahoo.com>
2022-12-08 12:54:48 +0200

    Lab9

--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -1,12 +1,24 @@
 using Gandi_Ariana_Lab7.Models;
+using Gandi_Ariana_Lab7;
+
 namespace Gandi_Ariana_Lab7;
 
 public partial class ListPage : ContentPage
 {
-	public ListPage()
-	{
-		InitializeComponent();
-	}
+    public ListPage()
+    {
+        InitializeComponent();
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        var shopl = (ShopList)BindingContext;
+
+        listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+    }
+    
+
     async void OnSaveButtonClicked(object sender, EventArgs e)
     {
         var slist = (ShopList)BindingContext;
@@ -19,6 +31,16 @@ public partial class ListPage : ContentPage
         var slist = (ShopList)BindingContext;
         await App.Database.DeleteShopListAsync(slist);
         await Navigation.PopAsync();
+    }
+   
+    async void OnChooseButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ProductPage((ShopList)
+       this.BindingContext)
+        {
+            BindingContext = new Product()
+        });
+
     }
 
 }
